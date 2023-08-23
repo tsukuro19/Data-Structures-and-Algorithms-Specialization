40b238ed4387ca228e62c5c7cb7bb2985d0f220b
Tsukuro <vongola392112@gmail.com>
2023-08-23 12:43:38 +0700

    not change

--- Algorithmic Toolbox/week2/Fibonacci Efficient.py
+++ Algorithmic Toolbox/week2/Fibonacci Efficient.py
@@ -1,10 +1,13 @@
 def FibList(n):
     F=[int (i) for i in range(0,n+1)]
-    F[0]=0
+    F[0]=1
     F[1]=1
     for i in range(2,n+1):
         F[i]=F[i-1]+F[i-2]
     return F[n]
 
 n=int(input())
-print(FibList(n))
+if n==0:
+    print(1)
+else:
+    print(FibList(n))
